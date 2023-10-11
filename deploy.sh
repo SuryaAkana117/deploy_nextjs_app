6c5c5404951036e946d68519b8050e77c2be273c
SuryaAkana117 <111066674+SuryaAkana117@users.noreply.github.com>
2023-10-11 11:23:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy_nextjs_app
 
 cd /home/ubuntu/deploy_nextjs_app
 git pull origin master
-nvm use v16
+#nvm use v16
 
 # Source Yarn environment (if installed globally)
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
