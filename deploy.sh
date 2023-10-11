43b16c007e13685316635bc7759c33b56131146a
SuryaAkana117 <111066674+SuryaAkana117@users.noreply.github.com>
2023-10-11 11:56:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy_nextjs_app
 git pull origin master
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads NVM
-sudo nvm use v16
+nvm use v16
 
 # Source Yarn environment (if installed globally)
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+#export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
-sudo yarn install &&
-sudo yarn run build &&
+yarn install &&
+yarn run build &&
 pm2 restart next_hello_world_app
